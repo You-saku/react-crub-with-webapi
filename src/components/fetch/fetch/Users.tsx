bc8f1b01bd6a2636e255c36311781bbb9675498c
You-saku <yousaku.dev19@gmail.com>
2023-01-03 18:53:45 +0900

    add: postできる

--- src/components/fetch/fetch/Users.tsx
+++ src/components/fetch/fetch/Users.tsx
@@ -1,8 +1,10 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, useRef} from 'react'
 import User from '../../../data/User'
 
 const Users = () => {
-    const [users, setPosts] = useState<User[]>([])
+    const [users, setPosts] = useState<User[]>([]);
+
+    // get
     useEffect(() => {
         fetch('http://localhost:80/api/users', 
         {
@@ -17,6 +19,34 @@ const Users = () => {
         });
     },[])
 
+    // post
+    // 各useRefはこちらを参考に →　https://zenn.dev/tns_00/articles/react-typescript-form-error-of-value
+    const postName = useRef<HTMLInputElement>(null!)
+    const postEmail = useRef<HTMLInputElement>(null!)
+    const postAge = useRef<HTMLInputElement>(null!)
+    const userCreate = (e: any) => {
+        e.preventDefault();
+        const postData = {
+            name: postName.current.value,
+            email: postEmail.current.value,
+            age: parseInt(postAge.current.value), // 何故かstringのままになってしまうので
+        };
+
+        console.log(postData);
+
+        if (window.confirm("ユーザーを作成しますか?")) {
+            fetch('http://localhost:80/api/users', 
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(postData),
+            });
+        }
+    };
+
+    // delete
     const userDelete = (userId: number) => {
         if (window.confirm("削除してもよろしいですか？")) {
             fetch(`http://localhost:80/api/users/${userId}`,
@@ -43,6 +73,13 @@ const Users = () => {
                     )
                 }
             </ul>
+            <h2>ユーザー作成</h2>
+            <form>
+                <input ref={postName} type="text" placeholder='名前'/>
+                <input ref={postEmail} type="text" placeholder='メールアドレス'/>
+                <input ref={postAge} type="number" placeholder='年齢'/>
+                <button onClick={userCreate}>作成</button>
+            </form>
         </div>
     )
 }
